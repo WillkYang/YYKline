ed412db0691142399e406f7ab0afade9cb26ef83
WillkYang <willkyang@yeeyuntech.com>
2020-07-13 21:03:43 +0800

    update: timeline style

--- Y_Kline/Y_Kline/Painter/YYTimeline/YYTimelinePainter.m
+++ Y_Kline/Y_Kline/Painter/YYTimeline/YYTimelinePainter.m
@@ -31,6 +31,7 @@
     CGFloat maxH = CGRectGetHeight(area);
     CGFloat unitValue = maxH/minMaxModel.distance;
     
+    __block CGPoint pointStart, pointEnd;
     YYTimelinePainter *sublayer = [[YYTimelinePainter alloc] init];
     sublayer.frame = area;
     UIBezierPath *path1 = [UIBezierPath bezierPath];
@@ -40,10 +41,16 @@
         CGPoint point1 = CGPointMake(x+w/2, maxH - (m.Close.floatValue - minMaxModel.min)*unitValue);
         if (idx == 0) {
             [path1 moveToPoint:point1];
+            pointStart = point1;
         } else {
             [path1 addLineToPoint:point1];
         }
+        if (idx == models.count - 1) {
+            pointEnd = point1;
+        }
     }];
+    
+    // 画线
     {
         CAShapeLayer *l = [CAShapeLayer layer];
         l.path = path1.CGPath;
@@ -53,6 +60,35 @@
         [sublayer addSublayer:l];
     }
     [layer addSublayer:sublayer];
+    
+    // 绘制纯色背景
+//    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+//    {
+//        UIBezierPath *path2 = [path1 copy];
+//        [path2 addLineToPoint:CGPointMake(pointEnd.x, maxH)];
+//        [path2 addLineToPoint: CGPointMake(pointStart.x, maxH)];
+//        [path2 closePath];
+//        maskLayer.path = path2.CGPath;
+//        maskLayer.frame = area;
+//        maskLayer.fillColor = [UIColor colorWithRed:0.3 green:0.3 blue:0.7 alpha:0.5].CGColor;
+//        [layer addSublayer:maskLayer];
+//    }
+
+    // 渐变背景色
+    {
+        CAShapeLayer *maskLayer = [CAShapeLayer layer];
+        UIBezierPath *path2 = [path1 copy];
+        [path2 addLineToPoint:CGPointMake(pointEnd.x, maxH)];
+        [path2 addLineToPoint: CGPointMake(pointStart.x, maxH)];
+        [path2 closePath];
+        maskLayer.path = path2.CGPath;
+        CAGradientLayer *bgLayer = [CAGradientLayer layer];
+        bgLayer.frame = area;
+        bgLayer.colors = @[(id)[UIColor colorWithRed:0.3 green:0.3 blue:0.7 alpha:0.5].CGColor, (id)UIColor.clearColor.CGColor];
+        bgLayer.locations = @[@0.3, @0.9];
+        bgLayer.mask = maskLayer;
+        [layer addSublayer:bgLayer];
+    }
 }
 
 @end
